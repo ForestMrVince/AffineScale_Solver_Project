ed18060a1cd7172bfa92858042bcc6c50b6d0f20
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-28 11:24:47 +0800

    Create Project.h
    上传历史代码。

---
+++ Inc/Project.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//C++标准开发环境
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+//工程头文件
+#include "Define.h"			//变量类型定义
+#include "Variables.h"		//全局变量定义
+#include "Matrix.h"			//矩阵运算函数
+#include "Solver.h"			//迭代运算函数
